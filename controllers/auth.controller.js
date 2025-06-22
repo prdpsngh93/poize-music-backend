e0a0677aa7cb0fda9cfee38a0bb3ceb22ec3fe55
prdpsngh93 <prdpsngh93@gmail.com>
2025-06-22 13:32:09 +0530

    error handling

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -8,8 +8,10 @@ exports.register = async (req, res) => {
     const { name, email, password } = req.body;
     const hash = await bcrypt.hash(password, 10);
     const user = await User.create({ name, email, password: hash });
+    console.log("user" , user)
     res.status(201).json({ user });
   } catch (err) {
+    console.log("err",err);
     res.status(400).json({ error: err });
   }
 };
