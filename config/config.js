498932e2dff4ccb16441ba8af591d748036daf1b
prdpsngh93 <prdpsngh93@gmail.com>
2025-07-11 15:30:06 +0530

    sad

--- config/config.js
+++ config/config.js
@@ -2,16 +2,12 @@ require('dotenv').config();
 
 module.exports = {
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
+    use_env_variable: process.env.DATABASE_URL,
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false, // For self-signed certificates (Aiven needs this)
+        rejectUnauthorized: false,
       },
     },
   },
