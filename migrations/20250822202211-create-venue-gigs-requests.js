5c0165cd6a46ea2c5486f500cb15ae50f09b5c07
prdpsngh93 <prdpsngh93@gmail.com>
2025-08-23 01:53:36 +0530

    asd

---
+++ migrations/20250822202211-create-venue-gigs-requests.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('venue_gigs_requests', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.literal('uuid_generate_v4()'),
+        allowNull: false,
+        primaryKey: true,
+      },
+      gig_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+      },
+      artist_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      message: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      venue_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('venue_gigs_requests');
+  },
+};
