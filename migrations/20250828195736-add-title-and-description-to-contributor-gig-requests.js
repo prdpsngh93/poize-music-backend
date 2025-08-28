e0f02bc957e6ba7763c9047faf226eaed45eea17
prdpsngh93 <prdpsngh93@gmail.com>
2025-08-29 01:30:06 +0530

    asd

---
+++ migrations/20250828195736-add-title-and-description-to-contributor-gig-requests.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("contributor_gig_requests", "title", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+    await queryInterface.addColumn("contributor_gig_requests", "description", {
+      type: Sequelize.TEXT,
+      allowNull: true,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn("contributor_gig_requests", "title");
+    await queryInterface.removeColumn("contributor_gig_requests", "description");
+  },
+};
